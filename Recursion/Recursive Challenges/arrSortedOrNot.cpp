376d0a9cc42e8b8bd01a1426d25e0a9530ac1750
Mandeep Dalavi <mandeepdalavi@gmail.com>
2020-12-04 23:20:11 +0530

    added program to check wheather array is sorted or not under Recursion Challenges

---
+++ Recursion/Recursive Challenges/arrSortedOrNot.cpp
@@ -0,0 +1,17 @@
+//Check if the Array is sorted or not
+#include<iostream>
+using namespace std;
+
+bool sorted(int arr[], int n){
+    if(n==1)
+        return true;
+    bool restArr = sorted(arr+1, n-1);
+    return (arr[0] < arr[1] && restArr);
+}
+
+int main(){
+    int n;
+    int arr[] = {1, 2, 3, 4, 5, 6, 7};
+    cout << sorted(arr, 7) << endl;
+    return 0;
+}
