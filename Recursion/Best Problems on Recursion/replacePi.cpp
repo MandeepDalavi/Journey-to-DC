7b0ae7946c09674fd68f7eef43dd70ae17a59e11
Mandeep Dalavi <mandeepdalavi@gmail.com>
2020-12-05 21:03:42 +0530

    added Replace pi with 3.14 under Best problems on Recursion

---
+++ Recursion/Best Problems on Recursion/replacePi.cpp
@@ -0,0 +1,21 @@
+// Replace "pi" with 3.14 in a string
+#include<iostream>
+using namespace std;
+
+void replacePi(string s){
+    if(s.length()==0)
+        return;
+    if(s[0]=='p' && s[1]=='i'){
+        cout << "3.14";
+        return replacePi(s.substr(2));
+    }
+    else{
+        cout << s[0];
+        return replacePi(s.substr(1));
+    }
+}
+
+int main(){
+    replacePi("pippppiiiipi");
+    return 0;
+}
