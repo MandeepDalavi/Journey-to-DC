05d6398c9c04e32879c110e9f045eef78c59d22c
MandeepDalavi <mandeepdalavi@gmail.com>
2021-05-22 18:52:50 +0530

    Pallindromic Pyramid Pattern Other Approach

--- pattern.cpp
+++ pattern.cpp
@@ -147,6 +147,21 @@ void pallindromePyramid(int n){
             cout << l << " ";                           
         cout<<endl;
     }
+
+    /*
+    for(int i=1; i<=n; i++){
+        int j, k;
+        for(j=1; j<=n-i; j++)
+            cout << "  ";
+        k=i;
+        for(;j<=n;j++)
+            cout << k-- << " ";
+        k=2;
+        for(;j<=n+i-1;j++)
+            cout << k++ << " ";
+        cout<<endl;>>
+    }
+    */
 }
 
 int main(){
